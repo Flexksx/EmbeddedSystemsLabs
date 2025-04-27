544e919b77e3947904791087803f62747799a231
Flexksx <ccretu@extole.com>
2025-04-27 14:24:02 +0300

    add libs

---
+++ motor_control/lib/Button/Button.cpp
@@ -0,0 +1,43 @@
+#include "Button.h"
+#include <Arduino.h>
+
+Button::Button(int pin) : pin(pin), lastState(HIGH), lastDebounceTime(0) {}
+
+void Button::setup()
+{
+    pinMode(pin, INPUT_PULLUP);
+}
+
+bool Button::isClicked()
+{
+    return !digitalRead(pin);
+}
+
+bool Button::isClickedDB()
+{
+    const int debounceDelay = 50; // ms
+    bool currentState = !digitalRead(pin);
+
+    if (currentState != lastState)
+    {
+        lastDebounceTime = millis();
+    }
+
+    if ((millis() - lastDebounceTime) > debounceDelay)
+    {
+        if (currentState && !buttonPressed)
+        {
+            buttonPressed = true;
+            lastState = currentState;
+            return true;
+        }
+    }
+
+    if (!currentState)
+    {
+        buttonPressed = false;
+    }
+
+    lastState = currentState;
+    return false;
+}
---
+++ motor_control/lib/Joystick/Joystick.h
@@ -0,0 +1,20 @@
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+#include <Arduino.h>
+
+class Joystick
+{
+public:
+    Joystick(int vrxPin, int vryPin, int thresholdLow = 300, int thresholdHigh = 700);
+    char *determineDirection(int x, int y);
+    int getXValue();
+    int getYValue();
+
+private:
+    int vrxPin;
+    int vryPin;
+    int thresholdLow;
+    int thresholdHigh;
+};
+
+#endif
